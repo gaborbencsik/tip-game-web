b7513ed2e0e12fa84331c336a6035d44e6d330f9
bencso <gabor.bencsik13@gmail.com>
2018-01-11 17:18:09 +0100

    add scoreCalculator

--- api/services/scoreCalculator.js
+++ api/services/scoreCalculator.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const uri = process.env.MONGO_URL;
 
 const User = require('../models/user.js');
+const Match = require('../models/match.js');
+const Tip = require('../models/tip.js');
+const _ = require('lodash');
 
 mongoose.Promise = global.Promise;
 
@@ -18,12 +21,40 @@ const url = 'http://api.football-data.org/v1/competitions/452/fixtures';
 class ScoreCalculator {
 
   calculate() {
+    User.find({}).then(users => {
+      users.forEach(user => {
 
+        let matchList = Match.find({});
+        let tipList = Tip.find({userId: user._id});
+
+        Promise.all([matchList, tipList]).then(values => {
+          let matches = values[0];
+          let tips = _.keyBy(values[1], 'matchId');
+          let totalScore = 0;
+
+          matches.forEach(match => {
+            let tipMatchId = tips[match.matchId];
+
+            if (tipMatchId === undefined) {
+              totalScore += 0
+            } else {
+              let score = this.countScore(match.homeGoals, match.awayGoals, tipMatchId.homeGoals, tipMatchId.awayGoals)
+              totalScore += score
+            }
+          });
+          this.setScore(user._id, totalScore);
+        });
+      });
+    }).catch(error => {
+      console.log(error);
+    });
   }
 
-  getUsers() {
-    User.find({}).then(users => {
-      console.log(users);
+  setScore(userId, score) {
+    User.findOneAndUpdate({_id: userId}, { $set: { score: score }}, {upsert: true}).then(function(user) {
+      User.find({_id: user._id}).then(function(user) {
+        console.log(user);
+      })
     });
   }
 
@@ -42,25 +73,7 @@ class ScoreCalculator {
     }
     return score
   }
-
-}
-
-let ApiClient = {
-  fetchData: function(url) {
-    return fetch(url, {
-      method: 'GET',
-      headers: { 'X-Auth-Token': 'e61a9ca95d4540aeb951e2c2ba44359d' }
-    }).then(response => {
-      return response.json();
-    }).catch(error => {
-      console.log(error);
-      return Promise.reject();
-    })
-  }
 }
 
 const calculator = new ScoreCalculator;
-calculator.getUsers();
-// ApiClient.fetchData(url)
-//   .then((data) => { manager.parseApi(data) })
-//   .catch(error => { console.log(error); });
+calculator.calculate();
