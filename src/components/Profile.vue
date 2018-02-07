0fabe04eeaeb7545dc01817d5e1059abda8f8ef8
bencso <gabor.bencsik13@gmail.com>
2018-02-07 14:21:06 +0100

    change favourite team name in select

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -73,7 +73,7 @@ export default {
       return this.$store.state.teams.map(team => {
         return {
           value: team.name,
-          text: team.shortName
+          text: team.name
         }
       })
     }
